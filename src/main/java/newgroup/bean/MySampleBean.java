1bc28e4bdded9c0e61a1d55f48de9c785936f232
shunya.kiyokawa <m0112164e3@edu.teu.ac.jp>
2017-05-05 03:53:46 +0900

    gradleファイルをpush。書式を整えたものをpush

--- src/main/java/newgroup/bean/MySampleBean.java
+++ src/main/java/newgroup/bean/MySampleBean.java
@@ -9,23 +9,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
-
 //コンポーネントとして認識させるためのアノテーション。インスタンスがBeanとして登録される
 @Component
 public class MySampleBean {
 	private int counter = 0;
 	private int max = 10;
 
-	//コンストラクタにAutowiredをつけ、Bean登録時の値にする
-	//コンポーネントにAutowiredがないと起動に失敗する
+	// コンストラクタにAutowiredをつけ、Bean登録時の値にする
+	// コンポーネントにAutowiredがないと起動に失敗する
 	@Autowired
 	public MySampleBean(ApplicationArguments args) {
-		//ApplicationArgumentsはアプリ（SpringApplication.run）が実行されたときに渡された引数
+		// ApplicationArgumentsはアプリ（SpringApplication.run）が実行されたときに渡された引数
 		List<String> files = args.getNonOptionArgs();
-		//getNonOptionArgsはアプリケーション実行時の引数をlistとして取り出せる
+		// getNonOptionArgsはアプリケーション実行時の引数をlistとして取り出せる
 		try {
 			max = Integer.parseInt(files.get(0));
-			//取り出した最初の要素をInteger.ParseIntで整数に変換し、maxフィールドに設定
+			// 取り出した最初の要素をInteger.ParseIntで整数に変換し、maxフィールドに設定
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		}
@@ -34,7 +33,7 @@ public class MySampleBean {
 	public int count() {
 		counter++;
 		counter = counter > max ? 0 : counter;
-		//counterがmaxより大きいなら0、小さないならcounterを返す
+		// counterがmaxより大きいなら0、小さないならcounterを返す
 		return counter;
 	}
 }
--- src/main/java/newgroup/dao/MyDataEntityDaoImpl.java
+++ src/main/java/newgroup/dao/MyDataEntityDaoImpl.java
@@ -11,89 +11,75 @@ import newgroup.entity.MyDataEntity;
 
 @Repository
 public class MyDataEntityDaoImpl implements MyDataEntityDaoIF<MyDataEntity> {
-	//MyDataEntityに対するDB操作
+	// MyDataEntityに対するDB操作
 	private static final long serialVersionUID = 1L;
 
 	private EntityManager entityManager;
-	//Entitymanagerクラスを補完するためのフィールドを用意している
+	// Entitymanagerクラスを補完するためのフィールドを用意している
 
-	public MyDataEntityDaoImpl(){
+	public MyDataEntityDaoImpl() {
 		super();
 	}
-	public MyDataEntityDaoImpl(EntityManager manager){
+
+	public MyDataEntityDaoImpl(EntityManager manager) {
 		entityManager = manager;
 	}
-	//EntityManagerはSpringDataJPAでDBアクセスを行う機能を提供する
-
+	// EntityManagerはSpringDataJPAでDBアクセスを行う機能を提供する
 
 	@Override
 	public List<MyDataEntity> getAll() {
 		Query query = entityManager.createQuery("from MyDataEntity");
-		//SQLのクエリーに相当する機能をもつクラス
-		//from MyDataEntityは、select * from MyDataEntityに相当する
+		// SQLのクエリーに相当する機能をもつクラス
+		// from MyDataEntityは、select * from MyDataEntityに相当する
 		List<MyDataEntity> list = query.getResultList();
-		//クエリーの結果をlistインスタンスとして取得する
+		// クエリーの結果をlistインスタンスとして取得する
 		entityManager.close();
 		return list;
 
 	}
+
 	@Override
 	public MyDataEntity findById(long id) {
-		return (MyDataEntity)entityManager.createQuery("from MyDataEntity where id = "
-			+ id).getSingleResult();
+		return (MyDataEntity) entityManager.createQuery("from MyDataEntity where id = " + id).getSingleResult();
 	}
 
 	@Override
 	public List<MyDataEntity> findByName(String username) {
-		return (List<MyDataEntity>)entityManager.createQuery("from MyDataEntity where username = "
-			+ username).getResultList();
+		return (List<MyDataEntity>) entityManager.createQuery("from MyDataEntity where username = " + username)
+				.getResultList();
 	}
 
-
 	@Override
-	public List<MyDataEntity> find(String fstr){
+	public List<MyDataEntity> find(String fstr) {
 		List<MyDataEntity> list = null;
 		Long fid = 0L;
-		//0LのLはlongを意味する。つけないとintとして扱われ、longで宣言したidで矛盾が生じる
+		// 0LのLはlongを意味する。つけないとintとして扱われ、longで宣言したidで矛盾が生じる
 		try {
 			fid = Long.parseLong(fstr);
 		} catch (NumberFormatException e) {
-			//e.printStackTrace();
+			// e.printStackTrace();
 		}
-		Query query = entityManager
-				.createNamedQuery("findWithName")
-				.setParameter("fusername", "%" + fstr + "%");
-		//CreateNamedQueryメソッドで、MyDataEntityで定義されたクエリーコードを読みこみQueryインスタンスを作成する
+		Query query = entityManager.createNamedQuery("findWithName").setParameter("fusername", "%" + fstr + "%");
+		// CreateNamedQueryメソッドで、MyDataEntityで定義されたクエリーコードを読みこみQueryインスタンスを作成する
 		list = query.getResultList();
 		return list;
 	}
+
 	/*
-	@Override
-	public List<MyDataEntity> find(String fstr){
-		List<MyDataEntity> list = null;
-		String qstr = "from MyDataEntity where id = :fid or username like :fusername or mail like :fmail";
-		//idが:fidと一致する場合、またはusernameが:fusernameに似ている場合、またはmailが:fmailに似ている場合
-		Long fid = 0L;
-		//0LのLはlongを意味する。つけないとintとして扱われ、longで宣言したidで矛盾が生じる
-		try {
-			fid = Long.parseLong(fstr);
-		} catch (NumberFormatException e) {
-			//e.printStackTrace();
-		}
-		Query query = entityManager.createQuery(qstr)
-				.setParameter("fid", fid)
-				.setParameter("fusername", "%" + fstr + "%")
-				.setParameter("fmail", fstr + "@%");
-		list = query.getResultList();
-		return list;
-	}
-*/
+	 * @Override public List<MyDataEntity> find(String fstr){ List<MyDataEntity>
+	 * list = null; String qstr =
+	 * "from MyDataEntity where id = :fid or username like :fusername or mail like :fmail"
+	 * ; //idが:fidと一致する場合、またはusernameが:fusernameに似ている場合、またはmailが:fmailに似ている場合
+	 * Long fid = 0L; //0LのLはlongを意味する。つけないとintとして扱われ、longで宣言したidで矛盾が生じる try {
+	 * fid = Long.parseLong(fstr); } catch (NumberFormatException e) {
+	 * //e.printStackTrace(); } Query query = entityManager.createQuery(qstr)
+	 * .setParameter("fid", fid) .setParameter("fusername", "%" + fstr + "%")
+	 * .setParameter("fmail", fstr + "@%"); list = query.getResultList(); return
+	 * list; }
+	 */
 	@Override
 	public List<MyDataEntity> findByAge(int min, int max) {
-		return (List<MyDataEntity>)entityManager
-				.createNamedQuery("findByAge")
-				.setParameter("min", min)
-				.setParameter("max", max)
-				.getResultList();
+		return (List<MyDataEntity>) entityManager.createNamedQuery("findByAge").setParameter("min", min)
+				.setParameter("max", max).getResultList();
 	}
 }
